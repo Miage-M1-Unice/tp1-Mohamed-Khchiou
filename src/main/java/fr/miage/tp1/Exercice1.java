78e9cb64c6892bcc47741fd4502b3a4edb4b9a01
Exenth <mohamed.khchiou@etu.unice.fr>
2019-02-11 14:57:27 +0100

    Print recursif file

--- src/main/java/fr/miage/tp1/Exercice1.java
+++ src/main/java/fr/miage/tp1/Exercice1.java
@@ -5,16 +5,52 @@ import java.util.Arrays;
 
 public class Exercice1 {
 
+    File file;
+
+    public Exercice1(File file) {
+        this.file = file;
+    }
+
+    private void printRecusifFile(File file){
+        System.out.println("Bablabla");
+
+        for (File f: file.listFiles()) {
+            if (f.isDirectory()) {
+                printRecusifFile(f);
+            } else {
+                System.out.println(f);
+            }
+        }
+    }
+
+//    //filter with independent class
+//    private void filterFile(File file, String filter){
+//        MyFilter myFilter = new MyFilter(filter);
+//
+//        for (File f: file.listFiles(myFilter)) {
+//            if (f.isDirectory()) {
+//                System.out.println(file.toString());
+//                printRecusifFile(file);
+//            } else {
+//                System.out.println(f);
+//            }
+//        }
+//    }
 
     public static void main(String[] args){
 
+
         String path = ".";
 
-        File file = new File(path);
+        Exercice1 exo1 = new Exercice1(new File(path));
 
         System.out.println("Begin");
 
-        System.out.println(Arrays.toString(file.list()));
+        System.out.println(Arrays.toString(exo1.file.list()));
+
+        exo1.printRecusifFile(exo1.file);
+
+        //exo1.filterFile(exo1.file, "*.java");
 
         System.out.println("End");
     }
